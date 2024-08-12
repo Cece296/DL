1e68f2976d1d99fe1a11811155b81efd7091a70b
Cece296 <cecilieveidiksen@gmail.com>
2024-08-12 13:57:38 +0200

    imports

--- Exercise5.py
+++ Exercise5.py
@@ -1,3 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+
 
 
 
